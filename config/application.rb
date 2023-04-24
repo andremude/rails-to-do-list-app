e07b3d81f437666be82aea64c6d5a949583f354d
Andres Munoz <andremude@hotmail.com>
2023-04-24 10:17:12 -0500

    set require master key to fale

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module TodoList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.require_master_key = false
 
     # Configuration for the application, engines, and railties goes here.
     #
